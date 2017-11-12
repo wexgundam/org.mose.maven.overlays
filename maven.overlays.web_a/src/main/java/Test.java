7871459cfa6a5b0699a62a1e4e270b93f22c159d
wexgundam <wexgundam@163.com>
2017-11-12 16:08:23 +0800

    更新

---
+++ maven.overlays.web_a/src/main/java/Test.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2017 弘远技术研发中心. All rights reserved
+ * Project Name:org.mose.maven.overlays
+ * Module Name:TODO:Module
+ */
+
+/**
+ * what:    (这里用一句话描述这个类的作用). <br/>
+ * when:    (这里描述这个类的适用时机 – 可选).<br/>
+ * how:     (这里描述这个类的使用方法 – 可选).<br/>
+ * warning: (这里描述这个类的注意事项 – 可选).<br/>
+ *
+ * @author TODO:Who created on 2017/11/12
+ */
+public class Test {
+
+}
